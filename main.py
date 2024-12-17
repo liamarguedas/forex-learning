cfa568562a9f61479d47fe34960e97708180c487
liam <iliamftw2013@gmail.com>
2024-12-17 12:43:00 -0300

    add: new files and working api

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from data import AlphaVantage
 
 def main():
+    """todo"""
 
-    data = AlphaVantage(from_pair="EUR", to_pair="USD")
+    data = AlphaVantage(from_pair="EUR", to_pair="USD", training_data=False)
     data.get()
 
 if __name__ == "__main__":
