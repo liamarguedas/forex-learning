c61419f9de9cca300c906b340b473455e9c07df3
liam <iliamftw2013@gmail.com>
2024-12-17 12:33:24 -0300

    add: working data api

--- data/production.py
+++ data/production.py
@@ -1,27 +1,38 @@
+from datetime import datetime
+from pathlib import Path
 import os
 import json
 import requests
 import pandas
-from datetime import datetime
-from pathlib import Path
+
+PATH = Path(__file__).parent
+
 
 class AlphaVantage:
-    def __init__ (self, from_pair, to_pair, timeline="FX_DAILY"):
+    """todo"""
+    def __init__ (self, from_pair, to_pair, training_data=False, timeline="FX_DAILY"):
 
         self.to_pair = to_pair
         self.from_pair = from_pair
         self.timeline = timeline
+        self.training_data = training_data
+        self.outputsize = "full" if self.training_data else "compact"
         self.api_key = os.getenv('alpha_vantage')
 
     @staticmethod
-    def log_metadata(source_metadata: dict):
+    def export_to_csv(json_file, name):
+        """todo"""
+        data = pandas.read_json(json_file).T
+        data.to_csv(name)
+
+    @staticmethod
+    def log_metadata(source_metadata: dict, path: Path ):
 
         """todo"""
-        logs_dir = Path(__file__).parent / "logs"
         now = datetime.now()
         file_name = now.strftime("%d-%m-%Y-%H%M%S") + ".txt"
 
-        with open(logs_dir / file_name, "a", encoding="utf-8") as log_file:
+        with open(path / file_name, "a", encoding="utf-8") as log_file:
             for key in source_metadata.keys():
                 log_file.write(f'{key.split(".")[1][1:]}: {source_metadata[key]}\n')
 
@@ -31,27 +42,34 @@ class AlphaVantage:
         from_symbol_call = f"&from_symbol={self.from_pair}"
         to_symbol_call = f"&to_symbol={self.to_pair}"
         api_key_call = f"&apikey={self.api_key}"
+        data_amount = f"&outputsize={self.outputsize}"
         api_call = "https://www.alphavantage.co/query?"
         response = requests.get(
-            api_call + function_call + from_symbol_call + to_symbol_call + api_key_call,
+            api_call + function_call + from_symbol_call + to_symbol_call + api_key_call + data_amount,
             timeout=10
         )
         return response.json()
 
 
-    def convert_to_table(self):
+    def create_data(self):
         """todo"""
-
         data = self.retrieve_data()
-        self.log_metadata(data['Meta Data'])
-        
-               # 'Time Series FX (Daily)':
-                # {'2024-12-06': {'1. open': '1.05846', '2. high': '1.06298', '3. low': '1.05419', '4. close': '1.05680'},
-        with open(Path(__file__).parent / "test.csv", "a", encoding="utf-8") as file:
+        now = datetime.now()
+
+        file_name = f"{self.from_pair}{self.to_pair}" + now.strftime("%d-%m-%Y %H-%M-%S")
+
+        save_path = PATH / "train" if self.training_data else PATH / "prod"
+
+        json_file = file_name + ".json"
+        csv_file = file_name + ".csv"
+
+        self.log_metadata(data['Meta Data'], save_path / "logs")
+
+        with open(save_path / "json" / json_file , "a", encoding="utf-8") as file:
             json.dump(data["Time Series FX (Daily)"], file)
 
-
+        self.export_to_csv(save_path / "json" / json_file, save_path / "csv" / csv_file)
 
     def get(self):
         """todo"""
-        self.convert_to_table()
+        self.create_data()
